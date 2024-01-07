0cc39b8fd795e96129bc176dcf2bf5857f747924
cryptohero92 <alwaysflexibleman@gmail.com>
2024-01-07 23:46:05 +0900

    working on login button

--- src/component/header/Login.tsx
+++ src/component/header/Login.tsx
@@ -1,8 +1,43 @@
+import { Box, Button, Modal, Typography } from "@mui/material"
+import { useState } from "react";
+
 export function Login() {
 	// need to show login button. 
+	// i want to 
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	const style = {
+		position: 'absolute' as 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	};
+
 	return (
-		<div>
-		Login
-		</div>
+		<>
+			<Button onClick={handleOpen}>Login</Button>
+			<Modal
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="modal-modal-title"
+			aria-describedby="modal-modal-description"
+			>
+			<Box sx={style}>
+				<Typography id="modal-modal-title" variant="h6" component="h2">
+				Text in a modal
+				</Typography>
+				<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+				Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+				</Typography>
+			</Box>
+			</Modal>
+		</>
 	)
 }
