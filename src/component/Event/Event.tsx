66a3f128de68558cc6f781117824ad55d6a0ec37
cryptohero92 <alwaysflexibleman@gmail.com>
2024-01-12 09:01:31 +0900

    working on dashboard

---
+++ src/component/Event/Event.tsx
@@ -0,0 +1,121 @@
+import { DialogContent, DialogContentText, DialogActions, Dialog, Box, Typography, Button, CardMedia } from '@mui/material';
+import { Link } from 'react-router-dom';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import CampaignIcon from '@mui/icons-material/Campaign';
+import { useState } from 'react';
+import { useLocalStorage } from 'usehooks-ts';
+
+export default function Event({event}: any) {
+    const [open, setOpen] = useState(false);
+    const [accessToken] = useLocalStorage<string>('accessToken', '')
+
+    const openDialog = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    const confirmDelete = () => {
+        // dispatch(removeEvent({id: event._id}));
+        setOpen(false);
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/events/${event._id}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+				'Content-Type': 'application/json',
+			},
+            method: 'DELETE'
+        }).then((response) => response.json());
+    };
+
+    const [openPublish, setOpenPublish] = useState(false);
+
+    const openPublishDialog = () => {
+        setOpenPublish(true);
+    }
+
+    const handleClosePublish = () => {
+        setOpenPublish(false);
+    }
+
+    const confirmPublish = () => {
+        // dispatch(publishEvent({id: event._id}));
+        setOpenPublish(false);
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/events/publish`, {
+            body: JSON.stringify({id: event._id}),
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+				'Content-Type': 'application/json',
+			},
+            method: 'POST'
+        }).then((response) => response.json());
+    }
+
+    return (
+        <Box>
+            <CardMedia
+                sx={{ height: 80, width:80, minWidth: 80, ml: '1rem' }}
+                image={event.image}
+            />
+            <Box sx={{ p: 0.6, ml: 0.5 }}>
+                <Typography sx={{ color: 'lightgray', fontSize: 12}}>
+                    {event.category}
+                </Typography>
+                <Typography>
+                    {event.title}
+                </Typography>
+            </Box>
+            <Link to={`/dashboard/update/${event._id}`}>
+                <EditIcon>edit</EditIcon>
+            </Link>
+            <DeleteForeverIcon onClick={() => openDialog()}>
+                delete
+            </DeleteForeverIcon>
+            <CampaignIcon onClick={() => openPublishDialog()}>
+                Publish
+            </CampaignIcon>
+            {open && (
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="Delete Event"
+                >
+                    <DialogContent style={{ width: 300 }}>
+                        <DialogContentText>Are you sure?</DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button autoFocus onClick={handleClose} color="primary">
+                        Cancel
+                        </Button>
+                        <Button onClick={confirmDelete} color="primary" autoFocus>
+                        Delete
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            )}
+
+            {openPublish && (
+                <Dialog
+                    open={openPublish}
+                    onClose={handleClosePublish}
+                    aria-labelledby="Publish Event"
+                >
+                    <DialogContent style={{ width: 300 }}>
+                        <DialogContentText>Are you sure?</DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button autoFocus onClick={handleClosePublish} color="primary">
+                        Cancel
+                        </Button>
+                        <Button onClick={confirmPublish} color="primary" autoFocus>
+                        Publish
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            )}
+        </Box>
+        
+    )
+}
--- src/page/Dashboard/Dashboard.tsx
+++ src/page/Dashboard/Dashboard.tsx
@@ -1,10 +1,58 @@
-import { Box } from '@mui/material'
+import { Box, Button, Grid, Typography } from '@mui/material'
+import { Link } from 'react-router-dom';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { useEffect, useState } from 'react';
+import Event from "../../component/Event/event"
 
 export default function Dashboard() {
+    // first need to get all events. these events are from mongodb.
+    // 
+    const [events, setEvents] = useState([])
+    const getAllEvents = () => {
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/events`)
+			.then((response) => response.json())
+			// If yes, retrieve it. If no, create it.
+			.then((events) =>
+				setEvents(events)
+			)
+			.catch((err: any) => {
+                console.error(err)
+            });
+    }
+
+    useEffect(() => {
+        getAllEvents();
+    }, [])
 
     return (
-      <Box>
-        Welcome to PLSpeak!
-      </Box>
+        <Box>
+            <Box sx={{p: 2, width: 1}}>
+                <Link to="/dashboard/create">
+                    <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<AddCircleIcon />}
+                    >
+                        Create Event
+                    </Button>
+                </Link>
+            </Box>
+            <Box
+                sx={{ flexGrow: 1 }}
+            >
+                
+                {events.length == 0 ? (
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Typography>No Events</Typography></Box>
+                ): (
+                    <Grid container spacing={2} sx={{p: 2}}>
+                        {events.map((event, index) => (
+                            <Grid item key={index} xs={12} sm={6} lg={4} xl={3}>
+                                <Event event={event} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                )}
+            </Box>
+        </Box>
     )
 }
