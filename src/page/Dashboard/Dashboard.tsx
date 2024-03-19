a7f75c31d06b180934867d96991ae324a4bc7641
cryptohero92 <alwaysflexibleman@gmail.com>
2024-03-19 14:16:21 +0900

    EventReporter inserted into Dashboard to add event result report functionality

--- src/page/Dashboard/Dashboard.tsx
+++ src/page/Dashboard/Dashboard.tsx
@@ -1,12 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
 import { Box, Button, Grid, Typography } from '@mui/material'
 import { Link } from 'react-router-dom';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { useEffect, useState } from 'react';
-import UnPublishedEvent from "../../component/Event/UnPublishedEvent"
+import { readContract, readContracts } from "@wagmi/core";
+import UnPublishedEvent from "../../component/Event/UnPublishedEvent";
+import EventReporter from "../../component/Event/EventReporter";
+import { updatePublishedEvent } from "../../feature/slices/eventSlice";
+import { RootState } from "../../app/store";
+import { config } from "../../wagmi";
+import CTFExchangeContract from "../../../../backend/src/artifacts/contracts/papaya/CTFExchangeContract.json"
+import { BigNumberish, formatUnits } from 'ethers'
+import { PublishedEventInfo } from "../../types";
 
 export default function Dashboard() {
     // first need to get all events. these events are from mongodb.
+    const dispatch = useDispatch();
     const [events, setEvents] = useState([])
+    const { publishedEvents } = useSelector((state: RootState) => state.eventKey)
+
     const getAllEvents = () => {
         fetch(`${import.meta.env.VITE_BACKEND_URL}/events`)
 			.then((response) => response.json())
@@ -24,15 +36,87 @@ export default function Dashboard() {
             return events.filter(
                 (event: any) => event._id != eventId
             );
-        })
+        });
+        updatePublishedEventList();
+    }
+
+    async function updatePublishedEventList() {
+        const eventUrls = await readContract(config, {
+          abi: CTFExchangeContract.abi,
+          address: CTFExchangeContract.address as `0x${string}`,
+          functionName: 'getEventUrls',
+        });
+        if (eventUrls) {
+          (eventUrls as Array<any>).forEach((ipfsUrl, indexInArray) => {
+              let item: any = {
+                ipfsUrl,
+                indexInArray
+              };
+
+              fetch(`https://gateway.pinata.cloud/ipfs/${ipfsUrl}`)
+                .then((response) => response.json())
+                .then(eventInfo => {
+                  item.title = eventInfo.title
+                  item.detail = eventInfo.detail
+                  item.image = eventInfo.image
+                  item.category = eventInfo.category
+                  item.endDate = eventInfo.endDate
+                  let promises = [];
+                  for (let i = 0; i < eventInfo.bettingOptions.length; i++) {
+                    const contractPromise = readContracts(config, {
+                      contracts: [
+                        {
+                          abi: CTFExchangeContract.abi,
+                          address: CTFExchangeContract.address as `0x${string}`,
+                          functionName: 'getBetAmountOfBettingOption',
+                          args: [eventInfo.bettingOptions[i]] 
+                        },
+                        {
+                          abi: CTFExchangeContract.abi,
+                          address: CTFExchangeContract.address as `0x${string}`,
+                          functionName: 'getResultOfBettingOption',
+                          args: [eventInfo.bettingOptions[i]]
+                        }
+                      ]                  
+                    }).then(res => ({
+                      ipfsUrl: eventInfo.bettingOptions[i],
+                      bet: Number(formatUnits(res[0].result as BigNumberish, 6)),
+                      result: Number(res[1].result)
+                    }))
+
+                    const ipfsPromise = fetch(`https://gateway.pinata.cloud/ipfs/${eventInfo.bettingOptions[i]}`).then((response) => response.json()).then(optionInfo => ({
+                      title: optionInfo.title,
+                      image: optionInfo.image
+                    }));
+
+                    promises.push(Promise.all([contractPromise, ipfsPromise])
+                      .then((results) => (Object.assign({}, ...results)))) 
+                  }
+                  Promise.all(promises)
+                    .then(bettingOptions => {
+                      item.bettingOptions = bettingOptions;
+                      dispatch(updatePublishedEvent(item as PublishedEventInfo))
+                    })
+                })
+                .catch(err => {
+                  console.error(err);
+                })
+          });
+        }
     }
 
     useEffect(() => {
         getAllEvents();
+        updatePublishedEventList();
     }, [])
 
     return (
         <Box>
+            <Box>
+                <Typography>
+                    ==== Unpublished Events ===
+                </Typography>
+            </Box>
             <Box sx={{p: 2, width: 1}}>
                 <Link to="/dashboard/create">
                     <Button
@@ -55,6 +139,29 @@ export default function Dashboard() {
                         {events.map((event, index) => (
                             <Grid item key={index} xs={12} sm={6} lg={4} xl={3}>
                                 <UnPublishedEvent event={event} removeEvent={removeEvent} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                )}
+            </Box>
+
+            <Box sx={{mt: 5}}>
+                <Typography>
+                    ==== Published Events ===
+                </Typography>
+            </Box>
+
+            <Box
+                sx={{ flexGrow: 1 }}
+            >
+                
+                {publishedEvents.length == 0 ? (
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Typography>No Published Events</Typography></Box>
+                ): (
+                    <Grid container spacing={2} sx={{p: 2}}>
+                        {publishedEvents.map((event, index) => (
+                            <Grid item key={index} xs={12} sm={6} lg={4} xl={3}>
+                                <EventReporter event={event} />
                             </Grid>
                         ))}
                     </Grid>
